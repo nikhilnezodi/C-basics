da4881dca0ecaf668dae041911c64a6eb1dd47b1
Nikhil Nambiar <63468012+nikhilnezodi@users.noreply.github.com>
2025-01-31 11:48:05 +0000

    Pointer concepts added

--- multip.c
+++ multip.c
@@ -2,14 +2,23 @@
 
 int main(void)
 {
-    int num,i,mul;  #Declara
+    int num,i,mul;  //Declara
     printf("Enter a number: ");
     scanf("%d",&num);
     printf("Table of %d \n",num);
     printf("======================\n");
+    /*
     for(i=1;i<=12;i++)
     {   mul = num*i;
         printf("%d * %d = %d\n",num,i,mul);
+    }
+    */
+    i=1;
+    while (i<=10)
+    {
+        mul = num*i;
+        printf("%d * %d = %d\n",num,i,mul);
+        i++;
     }
     return 0;
 }
